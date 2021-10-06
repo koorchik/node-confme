1c75acd7a4a5c1ed11a485c3635438fceeaa61cc
Joseph Ottinger <joeo@enigmastation.com>
2021-10-06 09:41:24 -0400

    adding tests

--- src/confme.js
+++ src/confme.js
@@ -1,57 +1,63 @@
-require("dotenv-defaults").config();
+require('dotenv-defaults').config();
 
-const JSON5 = require("json5")
-const fs = require("fs");
+const fs = require('fs');
+// eslint-disable-next-line more/no-numeric-endings-for-variables
+const JSON5 = require('json5');
 
 function confme(configPath, livrSchemaPath) {
-  const template = fs.readFileSync(configPath).toString();
-  const configStr = replace(template, process.env);
+    // eslint-disable-next-line no-sync
+    const template = fs.readFileSync(configPath).toString();
+    const configStr = replace(template, process.env);
 
-  let config = {};
+    let config = {};
 
-  try {
-    config = JSON5.parse(configStr);
-  } catch (error) {
-    console.error("CANNOT PARSE JSON5:", configStr);
-    throw error;
-  }
+    try {
+        config = JSON5.parse(configStr);
+    } catch (error) {
+        console.error('CANNOT PARSE JSON5:', configStr);
+        throw error;
+    }
 
-  if (livrSchemaPath) {
-    config = validateConfig(config, livrSchemaPath);
-  }
+    if (livrSchemaPath) {
+        config = validateConfig(config, livrSchemaPath);
+    }
 
-  return config;
+    return config;
 }
 
 function validateConfig(config, livrSchemaPath) {
-  const LIVR = require("livr");
-  const livrExtraRules = require("livr-extra-rules");
+    const LIVR = require('livr');
+    const livrExtraRules = require('livr-extra-rules');
 
-  const livrRules = JSON5.parse(fs.readFileSync(livrSchemaPath).toString());
-  const validator = new LIVR.Validator(livrRules, true);
-  validator.registerRules(livrExtraRules);
+    // eslint-disable-next-line no-sync
+    const livrRules = JSON5.parse(fs.readFileSync(livrSchemaPath).toString());
+    const validator = new LIVR.Validator(livrRules, true);
 
-  const validConfig = validator.validate(config);
+    validator.registerRules(livrExtraRules);
 
-  if (!validConfig) {
-    const error = {
-      FAILED_CONFIG: config,
-      ERRORS: validator.getErrors()
-    };
-    throw new Error(JSON.stringify(error, null, 2));
-  }
+    const validConfig = validator.validate(config);
 
-  return validConfig;
+    if (!validConfig) {
+        const error = {
+            FAILED_CONFIG : config,
+            ERRORS        : validator.getErrors()
+        };
+
+        // eslint-disable-next-line no-magic-numbers
+        throw new Error(JSON.stringify(error, null, 2));
+    }
+
+    return validConfig;
 }
 
 function replace(template, vars) {
-  return template.replace(/\{\{\s*(.+?)\s*\}\}/g, (match, p1) => {
-    if (vars.hasOwnProperty(p1)) {
-      return vars[p1];
-    } else {
-      throw new Error(`Variable "${p1}" not set!`);
-    }
-  });
+    return template.replace(/\{\{\s*(.+?)\s*\}\}/g, (match, p1) => {
+        if (vars.hasOwnProperty(p1)) {
+            return vars[p1];
+        }
+
+        throw new Error(`Variable "${p1}" not set!`);
+    });
 }
 
 module.exports = confme;
